7e3ef26c1fbdb6cdb480bc1772c2875e02adee08
Piotr Szubert <piotr.szubert1@wp.pl>
2019-05-31 22:29:34 +0200

    forkjoinpool added

---
+++ src/content/Program.java
@@ -0,0 +1,23 @@
+package content;
+
+import org.apache.poi.extractor.POITextExtractor;
+import org.apache.poi.ooxml.extractor.ExtractorFactory;
+import org.apache.poi.poifs.filesystem.*;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+
+	}
+
+}
